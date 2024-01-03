241ecd118b0e2cc87dbd6c53613f29ed94077c29
BarelyBear <kangas.otso@gmail.com>
2024-01-03 17:57:45 +0200

    Cleanup & Abstract hittable class

---
+++ rtweekend.h
@@ -0,0 +1,23 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radius(double degrees) {
+  return degrees * pi / 180.0;
+}
+
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
