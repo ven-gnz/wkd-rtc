8cd9f3611024f9889f330178cf061ae8a60a17ae
ven-gnz <kangas.otso@gmail.com>
2025-12-20 19:35:14 +0200

    integrated new vec3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "color.h"
+#include "vec3.h"
 
 int main() {
 
@@ -12,16 +14,15 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining" << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
+            auto pixel_color = color( double(i) / (image_width - 1),
+                double(j) / (image_height - 1),
+                0);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            write_color(std::cout, pixel_color);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone \n";
 }
