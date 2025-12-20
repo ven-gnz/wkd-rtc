5dd032b5c1f3d92d7fd9ef0294b96249ae40e56f
ven-gnz <kangas.otso@gmail.com>
2025-12-20 19:34:59 +0200

    Some vector3 utility stuff + neat aliasing concept with using

---
+++ color.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color)
+{
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	int rbyte = int(255.999 * r);
+	int gbyte = int(255.999 * g);
+	int bbyte = int(255.999 * b);
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
