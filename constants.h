010c2bc57c7361d6eed4364a6a552bb6483e16e1
ven-gnz <kangas.otso@gmail.com>
2026-01-09 11:17:32 +0200

    Some pseudorandom sutff

--- constants.h
+++ constants.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cmath>
+#include <cstdlib>
+#include <random>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -16,6 +18,20 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
 }
 
 
